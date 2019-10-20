28a49796c077861b44d9ab02af9a40c4bec5be4f
Janchorizo <jancho@usal.es>
2019-10-20 19:11:12 +0200

    Make default folder be the path from which it is executed.

--- datawaiter/defaults.py
+++ datawaiter/defaults.py
@@ -2,5 +2,5 @@ import os
 
 PORT = 5000
 USE_SESSIONS = False
-DATA_FOLDER = os.path.join(os.path.abspath(os.path.curdir), 'data')
+DATA_FOLDER = os.path.abspath(os.path.curdir)
 PERSISTENT_DATA = False
