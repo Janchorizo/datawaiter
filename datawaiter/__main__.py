d3ba2869028ab8c36fd63df75b082c2be7fa8f05
Janchorizo <jancho@usal.es>
2019-10-20 21:34:53 +0200

    Differentiate between the root folder and the used cache folder.

--- datawaiter/__main__.py
+++ datawaiter/__main__.py
@@ -5,11 +5,11 @@ from . import defaults
 
 @click.command()
 @click.option(
-    '--folder',
+    '--root-folder',
     '-f',
     type=click.Path(exists=True, writable=True, resolve_path=True),
-    default=defaults.DATA_FOLDER,
-    help=f'Folder to use for saving the uploaded and processed files. Defaults to {defaults.DATA_FOLDER}.',
+    default=defaults.ROOT_FOLDER,
+    help=f'Root folder where the {defaults.DATA_FOLDER} folder will be placed to hold the uploaded files. Defaults to {defaults.ROOT_FOLDER}.',
 )
 @click.option(
     '--initial-file',
@@ -27,6 +27,7 @@ from . import defaults
 @click.option(
     '--maintain/--remove',
     '-m/-r',
+    'persistent_data',
     default=defaults.PERSISTENT_DATA,
     help='Whether to preserve or not the uploaded files (and the session if applicable) after server shutdown. Defaults to ' \
     	+ ('preserve the uploaded data.' if defaults.PERSISTENT_DATA else 'remove all generated files and folders.')
@@ -37,10 +38,9 @@ from . import defaults
     default=defaults.PORT,
     help=f'The port to which to bind the server. Defaults to {defaults.PORT}.'
 )
-def main(folder, initial_file, session_based, maintain, port):
+def main(root_folder, initial_file, session_based, persistent_data, port):
 	from .server import create_app
-	print(folder, initial_file, session_based, maintain, port)
-	app = create_app()
+	app = create_app(root_folder, initial_file, session_based, persistent_data)
 	app.run(port=port)
 
 if __name__ == '__main__':
--- datawaiter/defaults.py
+++ datawaiter/defaults.py
@@ -2,5 +2,6 @@ import os
 
 PORT = 5000
 USE_SESSIONS = False
-DATA_FOLDER = os.path.abspath(os.path.curdir)
+ROOT_FOLDER = os.path.abspath(os.path.curdir)
+DATA_FOLDER = '.datawaiter_cache'
 PERSISTENT_DATA = False
